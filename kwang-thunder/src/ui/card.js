d9350fb1820485b29322883a30c40ae595ecfe99
chulmin <chysiss@naver.com>
2023-03-22 22:05:37 +0900

    initial

--- kwang-thunder/src/ui/card.js
+++ kwang-thunder/src/ui/card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "../css/card.module.css";
+import styles from "../css/card.module.css";
 
 const Card = (props) => {
   // 흰 바탕에 그림자 있는 wrapper component. 다른 컴포넌트를 감싸는 용도로 사용해주세요.
