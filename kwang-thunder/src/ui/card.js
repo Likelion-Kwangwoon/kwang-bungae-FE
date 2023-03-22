2d10aa6a86b3cf85bf87c65456c747d675e655d0
chulmin <chysiss@naver.com>
2023-03-22 21:55:15 +0900

    initial chulmin

--- kwang-thunder/src/ui/card.js
+++ kwang-thunder/src/ui/card.js
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "../css/card.module.css";
 
-const card = (props) => {
+const Card = (props) => {
   // 흰 바탕에 그림자 있는 wrapper component. 다른 컴포넌트를 감싸는 용도로 사용해주세요.
   return <div className={classes.card}>{props.children}</div>;
 };
 
-export default card;
+export default Card;
