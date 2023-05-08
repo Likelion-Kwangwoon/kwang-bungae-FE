333666ff627dff1f95cc9a98ea0a0d55f54cb72e
practice1 <shson1217@kw.ac.kr>
2023-05-08 13:23:11 +0900

    update

--- kwang-thunder/src/pages/paging/paging.js
+++ kwang-thunder/src/pages/paging/paging.js
@@ -25,48 +25,17 @@ const Paging = (props) => {
   // 나머지가 있는 놈들 => 몫 +1 ==>>>>> Math.ceil ==> setPage = Math.ceil(setData/itemsCountPerPage)
   // 나머지가 없으면 걍 그 몫에 넣어라!
 
-  return (
-    <Pagination
-      activePage={page}
-      itemsCountPerPage={11}
-      totalItemsCount={50}
-      pageRangeDisplayed={5}
-      prePageText={"<"}
-      nextPageText={">"}
-      onChange={handlePageChange}
-    />
-  );
+  // // return (
+  // //   <Pagination
+  // //     activePage={page}
+  // //     itemsCountPerPage={9}
+  // //     totalItemsCount={50}
+  // //     pageRangeDisplayed={5}
+  // //     prePageText={"<"}
+  // //     nextPageText={">"}
+  // //     onChange={handlePageChange}
+  // //   />
+  // );
 };
 
-/*
-  const [items,setItems]= useState([]);
-  const [count,setCount]=.useState([]);
-  const [currentpage,setCurrentpage] = useState([1]);
-  const [postPerPage] = useState(16);
-  const [indexOfLastPost, setIndexOfLastPost] = useState(0);
-  const [indexOfFirsttPost, setIndexOfFirstPost] = useState(0);
-  const [currentPosts, setCurrentPosts] = useState(0);
-
-  React.useEffect(() => {
-  setCount(items.length);
-  setIndexOfLastPost(currentpage * postPerPage);
-  setIndexOfFirstPost(indexOfLastPost - postPerPage);
-  setCurrentPosts(items.slice(indexOfFirstPost, indexOfLastPost));
-}, [currentpage, indexOfFirstPost, indexOfLastPost, items, postPerPage]);
-
-const setPage = (e) => {
-  setCurrentpage(e);
-};
-return (
-  {currentPosts && items.length > 0 ? (
-      currentPosts.map((item)=> (
-          <div>{item.title}</div>
-          <div>{item.content}</div>
-      ))
-     ) 
-     : <div>게시물이 없습니다.</div>
-	}
-     <Paging page={currentpage} count={count} setPage={setPage} />
-)
-*/
 export default Paging;
--- kwang-thunder/src/ui/mainCard.js
+++ kwang-thunder/src/ui/mainCard.js
@@ -3,131 +3,55 @@ import Card from "react-bootstrap/Card";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Paging from "../pages/paging/paging";
+import Pagination from "react-js-pagination";
 
 function MainCard(props) {
   const [cards, setCards] = useState([]);
   const [filteredCards, setFilteredCards] = useState([]);
   const [idxNumber, setIdxnumber] = useState(1);
-  // useEffect(() => {
-  //   const getCards = async () => {
-  //     try {
-  //       const cardList = await axios
-  //         .get(
-  //           "https://aae6c754-9791-46c8-a805-4d38ac740450.mock.pstmn.io/post/list/"
-  //         )
-  //         .then(function (response) {
-  //           setCards(response.data);
-  //         });
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-  //   getCards();
-  // }, []);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(1);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = cards.slice(indexOfFirstItem, indexOfLastItem);
+  useEffect(() => {
+    const getCards = async () => {
+      try {
+        const cardList = await axios
+          .get(
+            "https://aae6c754-9791-46c8-a805-4d38ac740450.mock.pstmn.io/post/list/"
+          )
+          .then(function (response) {
+            setCards(response.data);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getCards();
+  }, []);
 
-  const dummy = [
-    {
-      postId: 1,
-      title: "우리 같이 벚꽃 볼래?",
-      people: 555,
-      dday: "2023-05-07-12:31",
-      type: "play",
-      content: "hello world!, 우리 같이 벚꽃 볼래?",
-    },
-    {
-      postId: 2,
-      title: "피시방 ㄱ??",
-      people: 5,
-      dday: "2023-04-12-12:31",
-      type: "play",
-      content: "쓰리팝 ㄱㄱ??",
-    },
-    {
-      postId: 3,
-      title: "코노 ㄱ??",
-      people: 15,
-      dday: "2023-08-12-12:31",
-      type: "play",
-      content: "코노 ㄱㄱ??",
-    },
-    {
-      postId: 4,
-      title: "닭갈비 ㄱ??",
-      people: 11,
-      dday: "2023-03-12-12:31",
-      type: "food",
-      content: "닭갈비 ㄱㄱ??",
-    },
-    {
-      postId: 5,
-      title: "볼링 ㄱㄱ?",
-      people: 7,
-      dday: "2023-07-12-12:31",
-      type: "play",
-      content: "볼링 ㄱㄱ?",
-    },
-    {
-      postId: 6,
-      title: "볼링 ㄱㄱ?",
-      people: 7,
-      dday: "2023-07-12-12:31",
-      type: "play",
-      content: "볼링 ㄱㄱ?",
-    },
-    {
-      postId: 7,
-      title: "볼링 ㄱㄱ?",
-      people: 7,
-      dday: "2023-07-12-12:31",
-      type: "play",
-      content: "볼링 ㄱㄱ?",
-    },
-    {
-      postId: 8,
-      title: "쿠키런 길드원 모집",
-      people: 20,
-      dday: "2023-07-12-12:31",
-      type: "play",
-      content: "팡팡팡에서 길드원 모집합니다",
-    },
-    {
-      postId: 9,
-      title: "영화볼사람",
-      people: 2,
-      dday: "2023-07-12-12:31",
-      type: "play",
-      content: "잠실에서 영화볼사람~",
-    },
-    {
-      postId: 10,
-      title: "제육 ㄱㄱ",
-      people: 4,
-      dday: "2023-07-12-12:31",
-      type: "etc",
-      content: "제육 먹을사람~",
-    },
-  ];
-
-  const idxChangeHandler = (idx) => {
-    setIdxnumber(idx);
+  const renderCards = () => {
+    return currentItems.map((card) => {
+      return (
+        <div key={card.postId}>
+          <h2>{card.title}</h2>
+          <p>{card.people}</p>
+          <p>{card.dday}</p>
+          <p>{card.type}</p>
+          <p>{card.content}</p>
+        </div>
+      );
+    });
   };
 
-  // 1. 부모가 자식한테 props로 idxChangeHandler를 onClick={idxChangeHandler}로 전달
-  // 2. 자식은 props.onClick(page)로 다시 부모한테 전달이 됩니다
-  // 3. page가 idx로 들어와서 Idxnumber 값이 자식이 보낸 값으로 업데이트됩니다!!!!!!!!!!!!!!!!!
-
-  useEffect(() => {
-    setCards(dummy);
-    const newFiltered = cards.filter(
-      (item) => props.category === "all" || item.type === props.category
-    );
-    setFilteredCards(newFiltered);
-    idxChangeHandler();
-  }, [idxNumber]);
+  // const idxChangeHandler = (idx) => {
+  //   setIdxnumber(idx);
+  // };
 
   return (
     <>
-      {cards &&
+      {/*{cards &&
         cards
           .filter(
             (item) => props.category === "all" || item.type === props.category
@@ -176,8 +100,17 @@ function MainCard(props) {
                 </Card>
               </Link>
             );
-          })}
-      <Paging onClick={idxChangeHandler} />
+          })} */}
+      {renderCards()}
+      <Pagination
+        activePage={currentPage}
+        pageRangeDisplayed={5}
+        prevPageText={"<"}
+        nextPageText={">"}
+        onChange={(pageNumber) => setCurrentPage(pageNumber)}
+        totalItemsCount={cards.length}
+        itemsCountPerPage={itemsPerPage}
+      />
     </>
   );
 }
